8bfe0eaeb96f9d236781daf12c92d55751e92530
punam <jagtappunam19@gmail.com>
2022-05-28 18:47:43 +0530

    navbar complete

---
+++ sprint-1/day-1/assignment/assign1/src/component/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Navbar = () =>{
+    return ( <div className='main'>
+        <div className='parent'>
+  <div id='first'>
+<a href='#' id='mor'>LOGOBAKERY</a>
+<a href='#'>Service</a>
+<a href='#'>Project</a>
+<a href='#'>About</a>
+  </div>
+
+  <div id='second'>
+<button href='#'>Contact</button>
+  </div>
+
+
+        </div>
+
+    </div>
+    );
+};
+export default  Navbar;
